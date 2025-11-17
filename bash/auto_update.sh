3bb2bebb472d027e9c955189a312c28b3d2b6209
kbj <khadarbashajilan001@gmail.com>
2025-11-17 09:29:27 +0530

    auto update by detecting distribution using grep cmd

--- bash/auto_update.sh
+++ bash/auto_update.sh
@@ -1,16 +1,31 @@
-if [ -d /etc/apt ]; then
-    echo "This host is based on Debian or Ubuntu"
-    sudo apt update && sudo apt dist-upgrade -y
-elif [ -d /etc/pacman.d ]; then
-    echo "This host is based on Arch"
+# 1) using -d (is directory)
+#if [ -d /etc/apt ]; then
+#    echo "This host is based on Debian or Ubuntu"
+#    sudo apt update && sudo apt dist-upgrade -y
+#elif [ -d /etc/pacman.d ]; then
+#    echo "This host is based on Arch"
+#    sudo pacman -Syu
+#elif [ -d /etc/yum ]; then
+#    echo "This host is based on RHEL/CentOS/Fedora"
+#    sudo yum update -y
+#elif [ -d /etc/zypp ]; then
+#    echo "This host is based on openSUSE"
+#    sudo zypper update -y
+#else
+#    echo "Unknown package manager"
+#    exit 1
+#fi
+
+#2) using grep
+
+
+release_file=/etc/os-release
+
+if grep -q "Arch" $release_file; then
+    echo "This is Arch based distribution"
     sudo pacman -Syu
-elif [ -d /etc/yum ]; then
-    echo "This host is based on RHEL/CentOS/Fedora"
-    sudo yum update -y
-elif [ -d /etc/zypp ]; then
-    echo "This host is based on openSUSE"
-    sudo zypper update -y
-else
-    echo "Unknown package manager"
-    exit 1
+elif grep -q "Pop" $release_file || grep -q "Ubuntu" $release_file; then
+    #It might be pop or ubuntu. coz, using pipe(||)
+    sudo apt update
+    sudo apt dist-upgrade
 fi
